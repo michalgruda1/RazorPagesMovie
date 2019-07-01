754e1c01e22dfa98132e0992b54cdd8267aa0ff7
Michal Gruda <michal.gruda@sygenic.com>
2019-07-01 14:26:14 +0200

    Played with migrations a bit, code is in sync with tutorial, for migrations, use Initial, to get state required by tutorial

---
+++ Migrations/20190701120617_NullableReleaseDateAndPrice.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesMovie.Migrations
+{
+    public partial class NullableReleaseDateAndPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Movie",
+                nullable: true,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                nullable: true,
+                oldClrType: typeof(decimal));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Movie",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldNullable: true);
+        }
+    }
+}
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,8 +11,8 @@ namespace RazorPagesMovie.Models
 	{
 		public string ID { get; set; }
 		public string Title { get; set; }
-		[DataType(DataType.Date)] public DateTime ReleaseDate { get; set; }
+		[DataType(DataType.Date), DisplayName("Release date")] public DateTime ReleaseDate { get; set; }
 		public string Genre { get; set; }
-		public decimal Price { get; set; }
+		[DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)] public decimal Price { get; set; }
 	}
 }
