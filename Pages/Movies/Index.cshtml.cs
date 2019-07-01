927347f6ea006f06d17f030430f3a5ee8274f4da
Michal Gruda <michal.gruda@sygenic.com>
2019-07-01 16:32:03 +0200

    Added SearchString property, with Get parameter binding, XSS protection, UpperInvariant treatment, and Genre property, bind from Get, with XSS protection

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -1,28 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
 namespace RazorPagesMovie.Pages.Movies
 {
-    public class IndexModel : PageModel
-    {
-        private readonly RazorPagesMovie.Models.RazorPagesMovieContext _context;
+	public class IndexModel : PageModel
+	{
+		private readonly RazorPagesMovie.Models.RazorPagesMovieContext _context;
 
-        public IndexModel(RazorPagesMovie.Models.RazorPagesMovieContext context)
-        {
-            _context = context;
-        }
+		public IndexModel(RazorPagesMovie.Models.RazorPagesMovieContext context)
+		{
+			_context = context;
+		}
 
-        public IList<Movie> Movie { get;set; }
+		public IList<Movie> Movie { get; set; }
+		[RegularExpression(@"[^&<>\""'/]*$", ErrorMessage = "Wykryto niedozwolone znaki"), BindProperty(SupportsGet = true), DataType(DataType.Text)] public string SearchString { get; set; }
+		public SelectList Genres { get; set; }
+		[RegularExpression(@"[^&<>\""'/]*$", ErrorMessage = "Wykryto niedozwolone znaki"), BindProperty(SupportsGet = true)] public string MovieGenre { get; set; }
 
-        public async Task OnGetAsync()
-        {
-            Movie = await _context.Movie.ToListAsync();
-        }
-    }
+		public async Task OnGetAsync()
+		{
+			var movies = _context.Movie.Select(m => m);
+
+			if (!string.IsNullOrEmpty(SearchString) && SearchString.Trim().Length > 0)
+			{
+				movies = movies.Where(m => m.Title.ToUpperInvariant().Contains(SearchString.ToUpperInvariant()));
+			}
+
+			Movie = await movies.ToListAsync();
+		}
+	}
 }
