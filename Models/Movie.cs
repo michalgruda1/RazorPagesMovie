392b89c4451b5a99d0a3e7dd3251027c43324cf5
Michal Gruda <michal.gruda@sygenic.com>
2019-07-02 12:47:16 +0200

    Data annotation for validation added.

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -8,10 +8,29 @@ namespace RazorPagesMovie.Models
 	public class Movie
 	{
 		public string ID { get; set; }
+
+		[StringLength(60, MinimumLength = 3)]
+		[Required]
 		public string Title { get; set; }
-		[DataType(DataType.Date), DisplayName("Release date")] public DateTime ReleaseDate { get; set; }
+
+		[DataType(DataType.Date)]
+		[DisplayName("Release date")]
+		public DateTime ReleaseDate { get; set; }
+
+		[Required]
+		[RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+		[StringLength(30)]
 		public string Genre { get; set; }
-		[Column(TypeName = "decimal(18, 2)"), DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)] public decimal Price { get; set; }
+
+		[Column(TypeName = "decimal(18, 2)")]
+		[Range(1, 100)]
+		[DataType(DataType.Currency)]
+		[DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:C}")]
+		public decimal Price { get; set; }
+
+		[RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+		[StringLength(5)]
+		[Required]
 		public string Rating { get; set; }
 	}
 }
