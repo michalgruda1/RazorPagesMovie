87a4ce239da993f46c300b9a1426ee1ce1251b47
Michal Gruda <michal.gruda@sygenic.com>
2019-07-02 14:31:06 +0200

    Formatting

--- Startup.cs
+++ Startup.cs
@@ -30,15 +30,15 @@ namespace RazorPagesMovie
 		{
 			services.Configure<CookiePolicyOptions>(options =>
 			{
-							// This lambda determines whether user consent for non-essential cookies is needed for a given request.
-							options.CheckConsentNeeded = context => true;
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
 				options.MinimumSameSitePolicy = SameSiteMode.None;
 			});
 
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
-		    services.AddDbContext<RazorPagesMovieContext>(options =>
-		            options.UseSqlServer(Configuration.GetConnectionString("RazorPagesMovieContext")));
+			services.AddDbContext<RazorPagesMovieContext>(options =>
+				options.UseSqlServer(Configuration.GetConnectionString("RazorPagesMovieContext")));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
